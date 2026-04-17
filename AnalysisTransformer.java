d6e6655ffc85a04827e798dfb8a12da93da9a1ae
sahilshahane <sahilpshahane123@gmail.com>
2026-04-17 17:33:40 +0530

    feat: progress update

--- AnalysisTransformer.java
+++ AnalysisTransformer.java
@@ -1,11 +1,18 @@
 import java.util.*;
 import java.util.concurrent.ArrayBlockingQueue;
 
+import heros.FlowFunction;
+import heros.FlowFunctions;
+import heros.InterproceduralCFG;
+import heros.flowfunc.Identity;
+import heros.solver.IFDSSolver;
 import jas.Var;
 import soot.*;
 import soot.jimple.*;
+import soot.jimple.spark.SparkTransformer;
 import soot.jimple.toolkits.callgraph.CallGraph;
-import soot.jimple.toolkits.callgraph.Edge;
+import soot.jimple.toolkits.ide.DefaultJimpleIFDSTabulationProblem;
+import soot.jimple.toolkits.ide.icfg.JimpleBasedInterproceduralCFG;
 import soot.toolkits.graph.BriefUnitGraph;
 import soot.toolkits.graph.UnitGraph;
 
@@ -20,104 +27,314 @@ class Node {
         return this.id;
     }
 
-    static Node make_obj(String id) {
-        return new Node("O_" + id);
+    static Node make_obj(Type objType, int lineNumber) {
+        return new Node("O_" + objType.toString() + "_" + lineNumber);
     }
 
     static Node make_var(String id) {
         return new Node("V_" + id);
-    }\
+    }
 
     @Override
     public boolean equals(Object obj) {
         if (!(obj instanceof Node))
             return false;
+        return this.id.equals(((Node) obj).id);
+    }
 
-        (Node)obj
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id);
+    }
+
+    @Override
+    public String toString() {
+        return id;
     }
 }
 
-class Edge {
-    Node node1;
-    Node node2;
+class Fact {
+    Local local;
+    SootField fields[];
+    Node target;
 
-    private Edge(Node n1, Node n2) {
-        this.node1 = n1;
-        this.node2 = n2;
-    }
+    static Fact ZERO = new Fact(null, null, null);
 
-    static Edge make(Node n1, Node n2) {
-        return new Edge(n1, n2);
+    public Fact(Local n1, SootField[] fields, Node n2) {
+        this.local = n1;
+        this.target = n2;
+        this.fields = fields;
     }
 
     @Override
     public boolean equals(Object obj) {
-        if (!(obj instanceof Edge))
+        if (this == obj)
+            return true;
+
+        if (!(obj instanceof Fact))
             return false;
 
-        return 
+        return this.local.equals(((Fact) obj).local) &&
+                this.target.equals(((Fact) obj).target) &&
+                Arrays.equals(this.fields, ((Fact) obj).fields);
     }
 
+    @Override
+    public int hashCode() {
+        return (31 * Objects.hash(local, target)) + Arrays.hashCode(fields);
+    }
+
+    @Override
+    public String toString() {
+        if (this == Fact.ZERO)
+            return "ZERO_FACT";
+
+        return local + " " + fields + " " + target;
+    }
+}
+
+class PointsToProblem
+        extends DefaultJimpleIFDSTabulationProblem<Fact, InterproceduralCFG<Unit, SootMethod>> {
+
+    public PointsToProblem(InterproceduralCFG<Unit, SootMethod> icfg) {
+        super(icfg); // Pass the Call Graph wrapper to the parent
+    }
+
+    @Override
+    public Fact createZeroValue() {
+        // Create your special dummy seed fact here
+        return Fact.ZERO;
+    }
+
+    @Override
+    public Map<Unit, Set<Fact>> initialSeeds() {
+        // Start at the entry of the main method
+        SootMethod main = Scene.v().getMainMethod();
+        Unit start = main.getActiveBody().getUnits().getFirst();
+        return Collections.singletonMap(start, Collections.singleton(zeroValue()));
+    }
+
+    @Override
+    protected FlowFunctions<Unit, Fact, SootMethod> createFlowFunctionsFactory() {
+        // This is where you implement the "getNormalFlowFunction"
+        // logic we talked about earlier!
+        return new FlowFunctions<Unit, Fact, SootMethod>() {
+            @Override
+            public FlowFunction<Fact> getCallFlowFunction(Unit callStmt, SootMethod callee) {
+                // System.out.println("getCallFlowFunction " + callStmt);
+
+                return source -> {
+                    if (source == zeroValue())
+                        return Collections.singleton(zeroValue());
+
+                    Set<Fact> res = new HashSet<>();
+                    Stmt stmt = (Stmt) callStmt;
+                    InvokeExpr ie = stmt.getInvokeExpr();
+
+                    var formalParameters = Helper.getParameterLocals(callee);
+
+                    // 1. Handle the 'this' pointer (The receiver)
+                    if (ie instanceof InstanceInvokeExpr) {
+                        InstanceInvokeExpr iie = (InstanceInvokeExpr) ie;
+
+                        // If our fact is about the variable 't', map it to 'this' in A()
+                        if (source.local.equals(iie.getBase())) {
+                            res.add(new Fact(callee.getActiveBody().getThisLocal(), source.fields,
+                                    source.target));
+                        }
+                    }
+
+                    // Handle other parameters
+                    for (int i = 0; i < ie.getArgCount(); i++) {
+                        var callerArg = ie.getArg(i);
+
+                        if (source.local.equals(callerArg)) {
+                            res.add(new Fact(formalParameters.get(i), source.fields, source.target));
+                        }
+                    }
+
+                    return res;
+                };
+            }
+
+            @Override
+            public FlowFunction<Fact> getCallToReturnFlowFunction(Unit arg0, Unit arg1) {
+                // System.out.println("getCallToReturnFlowFunction " + arg0);
+                return Identity.v();
+            }
+
+            @Override
+            public FlowFunction<Fact> getNormalFlowFunction(Unit curr, Unit succ) {
+
+                // System.out.println("getNormalFlowFunction " + curr);
+
+                return source -> {
+                    Set<Fact> res = new HashSet<>();
+
+                    // 1. Keep the identity for existing facts (unless killed)
+                    res.add(source);
+
+                    var lineNumber = curr.getJavaSourceStartLineNumber();
+
+                    if (curr instanceof AssignStmt) {
+                        AssignStmt assignStmt = (AssignStmt) curr;
+                        Value lhs = assignStmt.getLeftOp();
+                        Value rhs = assignStmt.getRightOp();
+
+                        if (source == zeroValue()) {
+
+                            if (lhs instanceof Local && rhs instanceof NewExpr) { // i0 = new T();
+                                var newExpr = (NewExpr) rhs;
+
+                                res.add(new Fact((Local) lhs, new SootField[0],
+                                        Node.make_obj(newExpr.getType(), lineNumber)));
+                                // System.out.println(res);
+                            }
+                        }
+
+                        else { // handle non-zero fact
+                            if (lhs instanceof Local && rhs instanceof Local) {
+                                if (source.local.equals(rhs)) {
+                                    res.add(new Fact((Local) lhs, source.fields, source.target));
+                                }
+                            }
+                        }
+
+                    }
+
+                    return res;
+                };
+            }
+
+            @Override
+            public FlowFunction<Fact> getReturnFlowFunction(Unit callSiteStmt, SootMethod callee, Unit exitStmt,
+                    Unit returnSite) {
+
+                // System.out.println("getReturnFlowFunction " + arg0);
+
+                return source -> {
+                    if (source == zeroValue())
+                        return Collections.singleton(source);
+
+                    var res = new HashSet<Fact>();
+
+                    if (exitStmt instanceof ReturnStmt && callSiteStmt instanceof AssignStmt) {
+                        var assignStmt = (AssignStmt) callSiteStmt;
+                        var returnStmt = (ReturnStmt) exitStmt;
+
+                        var returnOp = returnStmt.getOp();
+
+                        Value callerLhs = assignStmt.getLeftOp();
+
+                        if (returnOp instanceof Local && callerLhs instanceof Local) {
+                            if (source.local.equals(returnOp)) {
+                                res.add(new Fact((Local) callerLhs, source.fields, source.target));
+                            }
+                        } else {
+                            // TODO: handle constant return values
+                        }
+
+                    }
+
+                    return res;
+                };
+            }
+        };
+    }
+}
+
+class Helper {
+    static public List<Local> getParameterLocals(SootMethod callee) {
+        List<Local> parameterLocals = new ArrayList<>();
+
+        if (!callee.hasActiveBody()) {
+            return parameterLocals; // Return empty if no body exists
+        }
+
+        Body body = callee.getActiveBody();
+        for (Unit u : body.getUnits()) {
+            if (u instanceof IdentityStmt) {
+                IdentityStmt is = (IdentityStmt) u;
+                Value rightOp = is.getRightOp();
+
+                // Check if the right side is a parameter reference (@parameterX)
+                if (rightOp instanceof ParameterRef) {
+                    Local l = (Local) is.getLeftOp();
+                    parameterLocals.add(l);
+                }
+            } else if (!(u instanceof IdentityStmt)) {
+                // Optimization: Once we hit a non-identity statement,
+                // we've passed the parameter declarations.
+                break;
+            }
+        }
+        return parameterLocals;
+    }
 }
 
 public class AnalysisTransformer extends SceneTransformer {
-    static CallGraph cg;
-
     @Override
     protected void internalTransform(String phaseName, Map<String, String> options) {
-        // Store the call graph once as a static field...
-        cg = Scene.v().getCallGraph();
+        CallGraph cg = Scene.v().getCallGraph();
 
-        // This code lets us get the main method, our testcases will only have one start
-        // point that is the main method
-        // in the Test class...
-        var entrypoints = Scene.v().getEntryPoints();
-        assert (entrypoints.size() == 1);
-        SootMethod entryMethod = entrypoints.get(0);
-
-        handleMainMethod(entryMethod);
-    }
-
-    void handleMainMethod(SootMethod method) {
-        var scalarReplaceableStats = new ScalarReplaceableStats();
-        var globals = new PTGraph();
-
-        // get all the global objects
-        var globalNodes = Helper.getAllGlobalNodes();
-
-        for (var node : globalNodes) {
-            globals.add_global_node(node);
+        if (cg.size() <= 0) {
+            throw new RuntimeException("Call graph is empty. Spark might not have run!");
         }
 
-        var result = Helper.performDataflowAnalysis(scalarReplaceableStats, method, null, globals, null);
+        // System.out.println("Total Edges in Call Graph: " + cg.size());
 
-        var objNodes = result.exitSet.get_nodes().stream()
-                .filter(node -> (node instanceof ObjectNode))
-                .sorted((n1, n2) -> {
-                    var l1 = ((ObjectNode) n1).object.creation_line_number;
-                    var l2 = ((ObjectNode) n2).object.creation_line_number;
-                    return Integer.compare(l1, l2);
-                }).toList();
+        JimpleBasedInterproceduralCFG icfg = new JimpleBasedInterproceduralCFG();
+        PointsToProblem problem = new PointsToProblem(icfg);
+        IFDSSolver<Unit, Fact, SootMethod, InterproceduralCFG<Unit, SootMethod>> solver = new IFDSSolver<>(problem);
 
-        for (var node : objNodes) {
-            if (!(node instanceof ObjectNode) || globals.containsNode(node.id))
-                continue;
+        // SootMethod main = Scene.v().getMainMethod();
+        // for (Unit u : main.getActiveBody().getUnits()) {
+        // if (((Stmt) u).containsInvokeExpr()) {
+        // System.out.println("Call site: " + u);
+        // System.out.println("Callees: " + icfg.getCalleesOfCallAt(u).size());
+        // }
+        // }
 
-            var objNode = (ObjectNode) node;
+        solver.solve();
 
-            var lineNums = scalarReplaceableStats.get_func_lineNumbers(node.id)
-                    .stream()
-                    .sorted((numStr1, numStr2) -> {
-                        return Integer.compare(Integer.valueOf(numStr1), Integer.valueOf(numStr2));
-                    }).toList();
+        for (SootClass sc : Scene.v().getApplicationClasses()) {
+            for (SootMethod sm : sc.getMethods()) {
+                if (sm.hasActiveBody()) {
+                    System.out.println("--- Method: " + sm.getSignature() + " ---");
+                    for (Unit u : sm.getActiveBody().getUnits()) {
+                        System.out.println("[Stmt] " + u);
 
-            var funcLineNumbers = objNode.object.is_scalar_replaceable ? String.join(",", lineNums) : "";
-
-            var printObjID = "O" + String.valueOf(objNode.object.creation_line_number);
-
-            System.out.println(printObjID + " = "
-                    + (objNode.object.is_scalar_replaceable ? "Y" + "[" + funcLineNumbers + "]" : "N"));
+                        // To get facts *after* the statement, we query the successors
+                        List<Unit> succs = icfg.getSuccsOf(u);
+                        if (succs.isEmpty()) {
+                            // E.g., for ReturnStmt, we might just use the results at the stmt itself
+                            // if there's no intra-procedural successor, though it technically represents
+                            // "before"
+                            Set<Fact> facts = solver.ifdsResultsAt(u);
+                            if (facts != null) {
+                                for (Fact f : facts) {
+                                    if (f != Fact.ZERO) {
+                                        System.out.println("    => (before exit) " + f);
+                                    }
+                                }
+                            }
+                        } else {
+                            // Print facts reaching the successors
+                            for (Unit succ : succs) {
+                                Set<Fact> facts = solver.ifdsResultsAt(succ);
+                                if (facts != null) {
+                                    for (Fact f : facts) {
+                                        if (f != Fact.ZERO) {
+                                            System.out.println("    => " + f);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
-    }
 
+    }
 }
--- PA4.java
+++ PA4.java
@@ -1,37 +1,86 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import soot.*;
+import soot.jimple.spark.SparkTransformer;
 import soot.options.Options;
 
 public class PA4 {
     public static void main(String[] args) {
-        String classPath = "./class_outputs/" + args[0];
+        String classPath = "./testcases/" + args[0];
 
-        // 1. Setting global options
+        G.reset();
+
+        // 2. Setting Global Options (The Options.v() way)
         Options.v().set_keep_line_number(true);
 
-        // 2. Add your transformer to the "wjtp" pack
-        SceneTransformer sceneTransformer = new AnalysisTransformer();
-        PackManager.v().getPack("wjtp").add(new Transform("wjtp.dfa", sceneTransformer));
+        Options.v().set_prepend_classpath(true); // Equivalent to -pp
+        Options.v().set_soot_classpath(classPath);
 
-        // 3. Prepare arguments
-        String[] sootArgs = {
-                "-cp", classPath,
-                "-pp", // sets the class path for Soot
-                "-w",
-                "-app",
-                "-allow-phantom-refs",
-                "-no-bodies-for-excluded",
-                "-exclude", "java.*",
-                "-exclude", "javax.*",
-                "-exclude", "sun.*",
-                "-exclude", "com.sun.*",
-                "-exclude", "jdk.*",
-                "-f", "J",
-                "-t", "1",
-                "-main-class", "Test", // specify the main class
-                "-process-dir", classPath
-        };
+        Options.v().set_process_dir(Collections.singletonList(classPath));
+        Options.v().set_app(true);
+        Options.v().set_whole_program(true);
 
-        // 4. Just call main. It will parse args, load classes, and run the packs.
-        soot.Main.main(sootArgs);
+        Options.v().set_src_prec(Options.src_prec_class);
+
+        Options.v().set_allow_phantom_refs(true);
+        Options.v().set_main_class("Test");
+        Options.v().set_output_format(Options.output_format_jimple);
+
+        // 2. Use the exclude list to mark everything else as Library
+        List<String> excluded = new ArrayList<>();
+        excluded.add("java.*");
+        excluded.add("javax.*");
+        excluded.add("sun.*");
+        excluded.add("com.sun.*");
+        excluded.add("jdk.*");
+        Options.v().set_exclude(excluded);
+
+        // 3. Crucial: Don't load bodies for excluded classes
+        Options.v().set_no_bodies_for_excluded(true);
+
+        // 3. Load Classes
+        Scene.v().addBasicClass("java.lang.Object", SootClass.SIGNATURES);
+        Scene.v().loadNecessaryClasses();
+
+        // 4. Force your test classes to be Application Classes
+        // This ensures Spark doesn't skip them
+        // BUG: package name conflict
+        for (SootClass sc : Scene.v().getClasses()) {
+            if (sc.getName().contains("testcases")) {
+                sc.setApplicationClass();
+            }
+        }
+
+        // 4. Force Call Graph Generation (Spark)
+        // This is the manual trigger that populates the Scene's CallGraph
+        Map<String, String> opt = new HashMap<>();
+        opt.put("enabled", "true");
+        opt.put("verbose", "true");
+        opt.put("on-fly-cg", "true");
+        opt.put("t", "0");
+        SparkTransformer.v().transform("cg.spark", opt);
+
+        Options.v().set_num_threads(1);
+
+        // 6. Verify Call Graph exists before starting Analysis
+        if (!Scene.v().hasCallGraph()) {
+            throw new RuntimeException("Spark failed to build a Call Graph!");
+        }
+
+        // 5. Register your Analysis
+        // We add it to 'wjtp' (Whole Jimple Transformation Pack)
+        AnalysisTransformer analysis = new AnalysisTransformer();
+        PackManager.v().getPack("wjtp").add(new Transform("wjtp.analysis", analysis));
+
+        // 6. Execute
+        // DO NOT call soot.Main.main(args) here.
+        PackManager.v().runPacks();
+
+        // Optional: Write out Jimple files to sootOutput/
+        PackManager.v().writeOutput();
     }
 }
