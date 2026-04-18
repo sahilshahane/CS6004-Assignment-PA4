888f2f25a4c2ad250a666641dabc04a63a461d33
sahilshahane <sahilpshahane123@gmail.com>
2026-04-19 04:15:46 +0530

    feat: store iterative_runs result

--- run_iterative.sh
+++ run_iterative.sh
@@ -9,15 +9,22 @@ fi
 
 RUN=1
 echo "========== RUN $RUN: $TEST_CASE =========="
+TEST_NAME=$(basename "$TEST_CASE")
+mkdir -p "iterative_runs/$TEST_NAME"
+rm -rf "iterative_runs/$TEST_NAME"/*
+
 ./run.sh "$TEST_CASE" --compile-only
 rm -rf sootOutput_iterative
 mv sootOutput sootOutput_iterative
+cp -r sootOutput_iterative "iterative_runs/$TEST_NAME/output_run$RUN"
 
 while true; do
     RUN=$((RUN + 1))
     echo ""
     echo "========== RUN $RUN: sootOutput_iterative =========="
     ./run.sh "sootOutput_iterative" --compile-only --no-build
+
+    cp -r sootOutput "iterative_runs/$TEST_NAME/output_run$RUN"
 
     if diff -rq sootOutput sootOutput_iterative > /dev/null; then
         echo "--> Fixpoint reached! No new changes found in run $RUN."
